059fe5d1d2327c3951f514c6abda804585b09e5b
Jignesh Patel <p.jignesh23@gmail.com>
2017-04-06 13:53:54 +0530

    Refactor counting sheep and test with small and large datasets

--- CountingSheep/CountingSheep.java
+++ CountingSheep/CountingSheep.java
@@ -10,55 +10,40 @@ import java.lang.Integer;
 public class CountingSheep{
 
 	public static void main(String[] args) throws Exception{
-		System.out.println("CountingSheep");
 
-		String inFile = "sample.in";
-		String outFile = "sample.out";
-
+		// String inFile = "sample.in";
+		// String outFile = "sample.out";
 		// String inFile = "A-small-practice.in";
-		// String outFile = "A-small-practice.out"
+		// String outFile = "A-small-practice.out";
+		String inFile = "A-large-practice.in";
+		String outFile = "A-large-practice.out";
 
 		Scanner sc = new Scanner(new File(inFile));
 		PrintWriter writer = new PrintWriter(outFile);
 
-		int caseNo = 0;
-
-		while(sc.hasNextInt()){
-			caseNo++;
-			int number = sc.nextInt();
-			System.out.println("Number : "+number);
-			writer.println("Case #"+ caseNo + ": " + logic(number));
+		int t = sc.nextInt();
+		for(int tc=1; tc<=t; tc++){
+			int n = sc.nextInt();
+			Set<Integer> digits = new TreeSet<>();
+			int last = 0;
+			if(n != 0){
+				for(int i=1; i<=Integer.MAX_VALUE; i++){
+					int next = last = n * i;
+					Set<Integer> nextDigits = new TreeSet<>();
+						while(next > 0) {
+							nextDigits.add((int)next%10);
+							next = next / 10;
+						}
+					digits.addAll(nextDigits);
+					if(digits.containsAll(Arrays.asList(0,1,2,3,4,5,6,7,8,9))){
+						break;
+					}
+				}
+			}
+			if(last == 0) writer.println("Case #"+ tc + ": INSOMNIA");
+			else writer.println("Case #"+ tc + ": " + last);
 		}
 		sc.close();
 		writer.close();
-
-	}
-
-	private static String logic(int number){
-		Set<Integer> digits = new TreeSet<>();
-		String result = "INSOMNIA";
-		if(number != 0){
-			for(int i=1; i<=Integer.MAX_VALUE; i++){
-				long nextNumber = number * i;
-				long temp = nextNumber;
-				Set<Integer> tempDigits = new TreeSet<>();
-
-					System.out.println("Number ("+number+") * i ("+i+") = nextNumber ("+nextNumber+")");
-					while(temp > 0) {
-						tempDigits.add((int)temp%10);
-						temp = temp / 10;
-					}
-					System.out.println("Temp Digits = "+ tempDigits);
-
-				digits.addAll(tempDigits);
-				System.out.println("Digits = "+ digits);
-				if(digits.containsAll(Arrays.asList(0,1,2,3,4,5,6,7,8,9))){
-					result = String.valueOf(nextNumber);
-					break;
-				}
-			}
-		}
-		System.out.println("Input - "+number+"   =>   Output - "+ result);
-		return result;
 	}
 }
